d949e26b0a203d76e688a81f867e9a9ffd7d6551
Daniele750 <danielpinc11@gmail.com>
2023-09-22 12:34:27 -0500

    Conf(Backend): configure ISS y Cors para solicitudes, configure launchsettings.josn

--- EmployeeCompany/EmployeeCompany/Program.cs
+++ EmployeeCompany/EmployeeCompany/Program.cs
@@ -1,13 +1,25 @@
+using EmployeeCompany.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
 builder.Services.AddControllers();
+var app = builder.Build();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,6 +29,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
